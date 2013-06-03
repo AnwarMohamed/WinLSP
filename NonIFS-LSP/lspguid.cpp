48e11cf4733cb5e2b468915d1424a2813fb027f8
AnwarMohamed <anwarelmakrahy@gmail.com>
2013-06-03 21:57:30 +0200

    initial commit

---
+++ NonIFS-LSP/lspguid.cpp
@@ -0,0 +1,60 @@
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// Copyright (C) 2004  Microsoft Corporation.  All Rights Reserved.
+//
+// Module Name: lspguid.cpp
+//
+// Description:
+//
+//      This module defines the GUID under which the LSP's hidden dummy
+//      entry is to be installed under. This GUID must be hard coded in
+//      this file so that the DLL instance can find itself in the Winsock 
+//      catalog. Once it finds the dummy hidden entry, it can then find
+//      the layered protocol entries belonging to the LSP (since these
+//      entries will reference the catalog ID of the hidden dummy entry).
+//      
+//      It is possible to create multiple LSPs by simply copying this project
+//      to another directory, changing the GUID in this file, and changing
+//      the DLL name. The LSP installer (instlsp.exe) can then install each
+//      LSP with the -d flag which is the full path to each LSP to install.
+//
+//      A new export has been added: GetLspGuid. This allows the LSP installer
+//      to install an arbitrary LSP as long as it exports this function as the
+//      installer needs to find the GUID to install the hidden dummy entry
+//      with. The GUIDs for the layered protocol entries are generated on the
+//      fly.
+//
+#include "lspdef.h"
+#include <windows.h>
+
+
+//
+// This is the hardcoded guid for our dummy (hidden) catalog entry
+//
+
+// {4F76EB9F-2A24-4b4e-B8C3-0F55D852281B}
+GUID gProviderGuid = { 0x4f76eb9f, 0x2a24, 0x4b4e, { 0xb8, 0xc3, 0xf, 0x55, 0xd8, 0x52, 0x28, 0x1b } };
+	
+
+//
+// Function: GetLspGuid
+//
+// Description:
+//      This function is exported by the DLL and it returns the GUID under
+//      which the LSP (hidden) entry is to be installed. Note that this export
+//      is not required to write an LSP but it is here to make the installer code
+//      easier since the LSP DLL and the installer at least need to know the GUID
+//      of the hidden dummy entry of the LSP. Using this export allows the installer
+//      to query each LSP instance of its GUID so that it may install it.
+//
+extern "C" __declspec(dllexport) void 
+//WSPAPI 
+GetLspGuid(
+    LPGUID lpGuid
+    )
+{
+    memcpy( lpGuid, &gProviderGuid, sizeof( GUID ) );
+}
